e03aaa13d8635574aff4ac6b04d52f936393276e
Hailong.Yao <hailong.yao@ipinyou.com>
2014-04-18 11:34:23 +0800

    update header file

--- pinyou-ios-sdk/Headers/PYConversion.h
+++ pinyou-ios-sdk/Headers/PYConversion.h
@@ -1,12 +1,14 @@
 //
-//  PinyouSDK.h
-//  PinyouSDK
+//  PYConversion.h
+//  AdLib
 //
-//  Created by darren on 14-2-19.
-//  Copyright (c) 2014年 ipinyou. All rights reserved.
+//  Created by darren on 14-4-16.
+//  Copyright (c) 2014年 lide. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <AdSupport/AdSupport.h>
+
 
 typedef enum : NSInteger {
 	PY_FirstRun = 0,
@@ -15,13 +17,12 @@ typedef enum : NSInteger {
     PY_Register
 } ConversionType;
 
-
-@interface PinyouSDK : NSObject
-
-+ (PinyouSDK *)defaultManager;
+@interface PYConversion : NSObject
++ (PYConversion *)defaultManager;
 
 //根据转化目标传递相应的转化类型ConversionType
 - (void)noticeConversionWithParamA:(NSString *)a conversionType:(ConversionType )type;
+
 
 @end
 
